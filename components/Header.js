8437097676211dafcd45b69ec17e1c9b05986f92
ejioguvictor <ejioguvictor6054@gmail.com>
2022-10-18 21:09:51 +0100

    created and styled the search bar in the header component

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
+import {SearchIcon} from '@heroicons/react/solid'
 
 const Header = () => {
   return (
-    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5'>
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image 
         src="https://links.papareact.com/qd3"
@@ -12,7 +13,13 @@ const Header = () => {
         objectPosition='left'
         />
       </div>
-      <div className="midside"></div>
+      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+        <input 
+        className='pl-5 bg-transparent outline-none flex-grow'
+        type="text" 
+        placeholder='Start your search here ...'/>
+        <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white p-2 rounded-full cursor-pointer md:mx-2'/>
+      </div>
       <div className="rightside"></div>
 
     </header>
