f37bf5a331c6c21d0104052633ac663ae331edde
ejioguvictor <ejioguvictor6054@gmail.com>
2022-10-18 21:35:56 +0100

    completed the header styling for the airbnb app

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import {SearchIcon} from '@heroicons/react/solid'
+import {GlobeAltIcon, UsersIcon, MenuIcon, UserCircleIcon} from '@heroicons/react/solid'
 
 const Header = () => {
   return (
@@ -15,12 +16,19 @@ const Header = () => {
       </div>
       <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
         <input 
-        className='pl-5 bg-transparent outline-none flex-grow'
+        className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400'
         type="text" 
-        placeholder='Start your search here ...'/>
+        placeholder='Search here ...'/>
         <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white p-2 rounded-full cursor-pointer md:mx-2'/>
       </div>
-      <div className="rightside"></div>
+      <div className="flex items-center justify-end space-x-4 text-gray-500">
+        <p className='hidden md:inline cursor-pointer'>Become a host</p>
+        <GlobeAltIcon className='h-6 cursor-pointer'/>
+        <div className='flex space-x-2 items-center border-2 p-2 rounded-full'>
+          <MenuIcon className='h-6'/>
+          <UserCircleIcon className='h-6' />
+        </div>
+      </div>
 
     </header>
   )
