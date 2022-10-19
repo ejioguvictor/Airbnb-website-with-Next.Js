33a566f3f83654ef09fcd6bca36c693f645bd266
ejioguvictor <ejioguvictor6054@gmail.com>
2022-10-19 13:20:39 +0100

    created and styled card component for the availabe spaces in the app

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ import Image from 'next/image'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import SmallCard from '../components/SmallCard'
+import Card from '../components/Card'
 
-export default function Home ({exploreData}) {
+export default function Home ({exploreData, cardsData}) {
   return (
     <div className="">
       <Head>
@@ -18,9 +19,22 @@ export default function Home ({exploreData}) {
       <main className='max-w-7xl mx-auto px-8 sm:px-16'>
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {exploreData.map(item => (
             <SmallCard key={item.img} img={item.img}  distance={item.distance} location={item.location}/>
           ))}
+          </div>
+        </section>
+        <section>
+          <h2 className='text-4xl font-semibold py-8 '>Live Anywhere</h2>
+           <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+           {cardsData?.map(item => (
+              <Card
+                key={item.img}
+               img={item.img} 
+               title={item.title}/>
+            ))}
+           </div>
         </section>
       </main>
     </div>
@@ -31,9 +45,13 @@ export async function getStaticProps() {
   const exploreData = await fetch('https://www.jsonkeeper.com/b/4G1G')
   .then((res) => res.json())
 
+  const cardsData = await fetch('https://www.jsonkeeper.com/b/VHHT')
+  .then((res) => res.json())
+
   return {
     props: {
-      exploreData
+      exploreData,
+      cardsData
     }
   }
 }
